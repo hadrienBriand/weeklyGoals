6b67b7e5ccacadfba6754ca9c62e1c33f09a257d
Hadrien Briand <hadrienbriand.pro@gmail.com>
2026-03-12 21:27:09 +0100

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.enableCors({
+app.enableCors({
     origin: [
-      'https://weekly-goals.netlify.app',
-      'http://localhost:5173',           
+      'https://weekly-goals.netlify.app', 
+      'http://localhost:5173',         
       'http://localhost:3000',
+      'https://weekly.wimsapp.com',      
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, 
   });
    app.useGlobalPipes(new ValidationPipe({
     transform: true,  
