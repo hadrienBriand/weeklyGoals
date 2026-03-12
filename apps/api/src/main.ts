6cd5c06b578b36b451beb95ac94eb58ca35805eb
Hadrien Briand <hadrienbriand.pro@gmail.com>
2026-03-12 21:15:45 +0100

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
-  origin: 'http://localhost:5173',  
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-})
-
+ app.enableCors({
+    origin: [
+      'https://weekly-goals.netlify.app',
+      'http://localhost:5173',           
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
    app.useGlobalPipes(new ValidationPipe({
     transform: true,  
     whitelist: true, 
